c7f5378e690b19a270a1b10f066a02b617e97a79
01101101 <sylvere.armange@gmail.com>
2014-04-27 12:53:52 +0200

    Player shadow

--- src/Player.hx
+++ src/Player.hx
@@ -1,4 +1,7 @@
 package ;
+
+import flash.display.BlendMode;
+import flash.display.Shape;
 import flash.geom.Rectangle;
 
 /**
@@ -9,11 +12,19 @@ import flash.geom.Rectangle;
 class Player extends AnimEntity {
 	
 	public var moving:Bool;
+	var shadow:Shape;
 	
 	public function new (mapX:Float = 0, mapY:Float = 0) {
 		super(mapX, mapY);
 		
 		moving = false;
+		
+		shadow = new Shape();
+		shadow.graphics.beginFill(0x000000, 0.2);
+		shadow.graphics.drawCircle(0, 8, 17);
+		shadow.graphics.endFill();
+		shadow.blendMode = BlendMode.DARKEN;
+		addChild(shadow);
 		
 		frames = new Array();
 		frames.push( { rect:new Rectangle(0, 0, 14, 11), duration:15 } );
