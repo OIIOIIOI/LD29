a2e29af5d04452e8b4eafc3fc47fa22304287d22
01101101 <sylvere.armange@gmail.com>
2014-04-28 20:49:35 +0200

    FPS

--- src/Main.hx
+++ src/Main.hx
@@ -2,6 +2,7 @@ package;
 
 import flash.display.Sprite;
 import flash.Lib;
+import openfl.display.FPS;
 
 /**
  * ...
@@ -16,20 +17,7 @@ class Main extends Sprite {
 		
 		// Add game
 		Lib.current.addChild(new Game());
-		
-		/*var dist = 10;
-		var angle = 27 * Math.PI / 180;
-		
-		var dx = dist * Math.cos(angle);
-		var dy = dist * Math.sin(angle);
-		trace(dx + " / " + dy);
-		
-		var dist2 = Math.sqrt(dx * dx + dy * dy);
-		trace(dist2);
-		
-		dx = 0;
-		var dist2 = Math.sqrt(dx * dx + dy * dy);
-		trace(dist2);*/
+		Lib.current.addChild(new FPS(0, 0, 0xFFFFFF));
 	}
 	
 }
